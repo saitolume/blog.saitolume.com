de11d713bd0246539c2389452e9ded78b5a601e3
Tadao Iseki <saitoeku3@gmail.com>
2020-12-30 01:52:49 +0900

    feat: meta description を追加

--- src/components/common/Head.tsx
+++ src/components/common/Head.tsx
@@ -28,6 +28,7 @@ const Head: VFC<HeadProps> = (props) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@saitolume" />
       <meta name="twitter:description" content={props.description} />
+      <meta name="description" content={props.description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </NextHead>
   )
