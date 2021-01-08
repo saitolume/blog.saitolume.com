671d437a821a938b245383f413deec65f34527be
Tadao Iseki <saitoeku3@gmail.com>
2021-01-08 20:58:24 +0900

    feat: フッターに Google Analytics の説明を追記

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -5,7 +5,18 @@ const Layout: FC = ({ children }) => (
   <>
     <main className={container}>{children}</main>
     <footer className={footer}>
-      <small>© 2020 saitolume</small>
+      <small>
+        このサイトではアクセス解析のために Cookie を使用した
+        <a
+          href="https://policies.google.com/technologies/partner-sites"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {` `}Google Analytics{` `}
+        </a>
+        を使用しています。
+      </small>
+      <small>© 2020 - 2021 saitolume</small>
     </footer>
   </>
 )
@@ -17,11 +28,16 @@ const container = css`
 
 const footer = css`
   width: 100%;
-  min-height: 64px;
+  min-height: 120px;
   align-items: center;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   margin-top: auto;
+
+  @media (max-width:480px) {
+    min-height: 160px;
+  }
 `
 
 export default Layout
